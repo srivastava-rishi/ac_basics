443564e224064babc74e4a002097c957f7e1d19f
Rishi Srivastava <rishi.srivastava@moneyview.in>
2025-05-28 19:56:19 +0530

    base setup

---
+++ app/src/main/java/com/rishi/androicomponents/activity/HomeActivity.kt
@@ -0,0 +1,15 @@
+package com.rishi.androicomponents.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.rishi.androicomponents.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
